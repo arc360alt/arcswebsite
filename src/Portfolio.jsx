3e9d193a2414fc35e9bc76d3f7707efdff999c17
arc360alt <voidyt16@gmail.com>
2026-01-11 01:18:04 -0600

    Nvm that was not the last change

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -457,7 +457,7 @@ const scrollToSection = (sectionRef, sectionName) => {
                   onChange={(e) => setShowForks(e.target.checked)}
                   className="sr-only peer"
                 />
-                <div className="w-11 h-6 bg-slate-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                <div className="w-11 h-6 bg-slate-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-600"></div>
               </div>
             </label>
             <label className="flex items-center gap-3 cursor-pointer">
